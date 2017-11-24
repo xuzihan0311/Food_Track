c9481c56d52b4aca21dcb40c2a5acd44d1b0d25f
kim nguyen <nguyen.thi.kd@gmail.com>
2017-11-24 15:32:03 -0500

    apps folder added

---
+++ fTrack/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "my-app",
+  template: `
+    <ActionBar title="My App" class="action-bar"></ActionBar>
+    <Image src="~/images/apple.jpg"></Image>
+  `
+})
+export class AppComponent {
+  // Your TypeScript logic goes here
+}
