e2e5a3eed589f9c4250cfd9cefdf3764fd5bd3b0
kim nguyen <nguyen.thi.kd@gmail.com>
2017-11-27 15:35:14 -0500

    schema added

--- fTrack/app/services/firebase.service.ts
+++ fTrack/app/services/firebase.service.ts
@@ -7,6 +7,32 @@ import firebase = require("nativescript-plugin-firebase");
 export class FirebaseService {
     add() {
         console.log("HELLO WORLD");
-        return firebase.push("/user", {"Name": "Kim"});
+        return firebase.push("/user",
+            {
+                'users': {
+                    'userID': 'userID123',
+                    'userProfile': {
+                        'firstName': 'Jane',
+                        'lastName': 'Doe',
+                        'profilePic': 'https://picture.com/jane.jpg',
+                    },
+                    'healthInfo': {
+                        'currentBP': '120/70',
+                        'last7BP': [1, 2, 3, 4, 5, 6, 7],
+                    },
+                    'recipeList': {
+                        'myRecipes': [],
+                        'myComments': [],
+                        'myRatings': []
+                    },
+                    'noifications': [],
+                },
+                'system': {
+                    'RecipesWithComments': [],
+                    'healthTips': [],
+                    'mostPopularRecipes': [],
+                }
+            }
+        );
     }
 }
